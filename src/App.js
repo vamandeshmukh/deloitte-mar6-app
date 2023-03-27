cf0fdaf48e100216750f73c130c05b550a9df17c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-27 16:48:02 +0530

    react-js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@
 
 // jsx 
 
-
-
 // function component 
+import Header from "./components/Header";
+
 const App = () => {
 
-  const salary = 100;
+  const salary = 100; // model / data 
 
-  return (
+  return ( // view 
     <div>
+      <Header />
       <h1>Deloitte React App</h1>
       <p>Welcome to Deloitte ReactJS Application.</p>
       <p>{salary}</p>
-      <p>{10 + 10}</p>
-      <p>{salary * 2}</p>
     </div>
   );
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+
+    return (
+        <div>
+            <p>This is header component.</p>
+        </div>
+    );
+};
+export default Header;
