bca0a13fbbac9f3aae59e1e8f51ed39a0ff52efb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-28 17:18:01 +0530

    react-js

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,11 +2,13 @@
 const Child = (props) => {
 
     const parentDataInChild = props.passData;
+    const childDataInChild = 'Monu';
 
     return (
         <div>
             <p>Child -</p>
             <p>Parent data in child: {parentDataInChild}</p>
+            <p>Child data in child: {childDataInChild}</p>
         </div>
     );
 
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -3,11 +3,14 @@ import Child from "./Child";
 const Parent = () => {
 
     const parentDataInParent = 'Sonu';
+    let childDataInparent = '';
 
     return (
         <div>
             <p>Parent -</p>
             <p>Parent data in parent: {parentDataInParent}</p>
+            <p>Child data in parent: {childDataInparent}</p>
+
             <Child passData={parentDataInParent} />
         </div>
     );
