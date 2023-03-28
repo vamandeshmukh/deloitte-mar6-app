3aeef731354944c0ed6c38fdc786f73d526add22
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-28 11:58:09 +0530

    react-js

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,6 @@ const Login = () => {
                     <br />
                     <input type='submit' value='Login' onChange={handleLogin} />
                 </form>
-
             </div>
         </div>
     );
