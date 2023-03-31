55209bad3e3738ceaa2063de724cf46f518da8ef
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-31 11:34:23 +0530

    react-js

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+
+// steps to use redux in react app 
+// 1. create store 
+// 2. provide store to react app (in index.js)
+// 3. create slices for compoments 
+// 4. invoke slices into components 
+
+// useful links :
+// https://redux.js.org/introduction/getting-started
+// https://react-redux.js.org/introduction/getting-started
+// https://redux-toolkit.js.org/introduction/getting-started
+
+
+const store = '';
+
+export default store;
